bcc70516f91ee8011c0ccc2872cdb4755a3b8b4b
AadithyaPradeep <aadi.prad@gmail.com>
2026-07-23 20:10:29 +0530

    change2

--- GMTK26 V1/Assets/Scripts/GrabCluck.cs
+++ GMTK26 V1/Assets/Scripts/GrabCluck.cs
@@ -33,18 +33,25 @@ public class GrabCluck : MonoBehaviour
                 grabbedCluck.GetComponent<ChickenWander>().enabled = true;
                 grabbedCluck.GetComponent<BoxCollider2D>().enabled = true;
                 grabbedCluck.SetParent(null);
+                float offset = (GetComponent<SpriteRenderer>().flipX) ? -2 : 2;
+                grabbedCluck.transform.position = new Vector2(transform.position.x + offset, transform.position.y);
                 grabbedCluck = null;
                 aimAnimator.SetBool("Grabbed",false);
-                grabbedCluck.transform.position = new Vector2(transform.position.x + (0.5f * Convert.ToInt32(GetComponent<SpriteRenderer>().flipX)), transform.position.y);
+             
 
             }
-            grabbedCluck = hc.GetSelectedClucks().transform;
-            grabbedCluck.SetParent(transform);
-            grabbedCluck.localPosition = new Vector3(0,0.3f,0);
-            grabbedCluck.GetComponent<BoxCollider2D>().enabled = false;
-            grabbedCluck.GetComponent<ChickenWander>().enabled = false;
-            aimAnimator = grabbedCluck.GetComponent<Animator>();
-            aimAnimator.SetBool("Grabbed",true);
+            else
+            {
+                grabbedCluck = hc.GetSelectedClucks().transform;
+                grabbedCluck.SetParent(transform);
+                grabbedCluck.localPosition = new Vector3(0, 0.3f, 0);
+                grabbedCluck.GetComponent<BoxCollider2D>().enabled = false;
+                grabbedCluck.GetComponent<ChickenWander>().enabled = false;
+                aimAnimator = grabbedCluck.GetComponent<Animator>();
+                aimAnimator.SetBool("Grabbed", true);
+
+            }
+            
         }
         
        
